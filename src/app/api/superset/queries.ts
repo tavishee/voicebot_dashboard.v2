d4f86086dc5f382b2418753258e69c5fec653df4
tavishee <tavishee@users.noreply.github.com>
2026-07-10 09:57:29 +0000

    fix(queries): add dl_last_updated upper bound to marketplace conversion query

--- src/app/api/superset/queries.ts
+++ src/app/api/superset/queries.ts
@@ -39,7 +39,9 @@ conversions AS (
       'Compulsory Personal Accident 2W - Standalone'
     )
     AND a.dl_last_updated >= date('${date}')
+    AND a.dl_last_updated < date('${date}') + interval '2' day
     AND b.dl_last_updated >= date('${date}')
+    AND b.dl_last_updated < date('${date}') + interval '2' day
     AND date(a.created_at) = date('${date}')
     AND CAST(a.customer_id AS VARCHAR) IN (SELECT lead_id FROM qualified_leads)
 )
